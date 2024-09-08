da4630db34e864c52ce9ea7a27b6eaa23876769f
Jihad Omer <jihadomer940@gmail.com>
2024-09-08 23:47:25 +0100

    Second test - scissors vs. rock

--- index.test.js
+++ index.test.js
@@ -1,8 +1,6 @@
-function rps() {
-     return "left";
+function rps(left) {
+  return left === "rock" ? "left" : "right";
 }
-
-
 
 describe("rock, paper, scissors", () => {
   it("should say left wins for rock vs. scissors", () => {
@@ -12,5 +10,14 @@ describe("rock, paper, scissors", () => {
     const outcome = rps(left, right);
 
     expect(outcome).toBe("left");
+  });
+
+  it("should say right wins for scissors vs. rock", () => {
+    const left = "scissors";
+    const right = "rock";
+
+    const result = rps(left, right);
+
+    expect(result).toBe("right");
   });
 });
